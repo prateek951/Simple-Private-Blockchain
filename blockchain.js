2c680d9ccba7bf513b1fdda36d472e3089bf6aa7
prateek951 <prince.cenation@gmail.com>
2019-01-19 16:33:41 +0530

    computing the block hash

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 /**
  * Based on block data model
  * Each block will have a hash, height, body, time and previousBlockHash
@@ -14,20 +16,24 @@ class Block {
 }
 /**
  * ================== Blockchain class ======================
- * Class with a constructor for the new blockchain 
+ * Class with a constructor for the new blockchain
  * ==========================================================
  */
 class Blockchain {
-    constructor() {
-        this.chain = [];
-    }
-/**
-* ==========================================================
-* Method to add a new block to the blockchain 
-* ==========================================================
-*/
+  constructor() {
+    this.chain = [];
+    this.addABlock(new Block("First Block in the chain"));
+  }
+  /**
+   * ==========================================================
+   * Method to add a new block to the blockchain
+   * ==========================================================
+   */
   addABlock(newBlock) {
+    if(this.chain.length > 0) {
+        newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
+    }
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     this.chain.push(newBlock);
   }
-
 }
