7eb9d27d674db47ae03e4f62f858bd8da3c27cea
prateek951 <prince.cenation@gmail.com>
2019-01-19 16:58:52 +0530

    added leveldb for persistence of data in the blockchain

--- blockchain.js
+++ blockchain.js
@@ -30,8 +30,13 @@ class Blockchain {
    * ==========================================================
    */
   addABlock(newBlock) {
-    if(this.chain.length > 0) {
-        newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
+    newBlock.height = this.chain.length;
+    newBlock.time = new Date()
+      .getTime()
+      .toString()
+      .slice(0, -3);
+    if (this.chain.length > 0) {
+      newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash;
     }
     newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
     this.chain.push(newBlock);
